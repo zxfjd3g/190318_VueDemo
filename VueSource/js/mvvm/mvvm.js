27801dff5603448ad08bdb9b74d01958f661818c
zxfjd3g <258147149@qq.com>
2019-08-03 17:15:54 +0800

    debug

--- VueSource/js/mvvm/mvvm.js
+++ VueSource/js/mvvm/mvvm.js
@@ -1,10 +1,15 @@
+/* 
+相当于Vue的构造函数
+*/
 function MVVM(options) {
+    // 保存配置对象到vm上
     this.$options = options;
+    // 保存data对象保存到vm和data变量上
     var data = this._data = this.$options.data;
+    // 保存vm到me变量
     var me = this;
 
-    // 数据代理
-    // 实现 vm.xxx -> vm._data.xxx
+    // 遍历data每个属性, 并对属性实现数据代理
     Object.keys(data).forEach(function(key) {
         me._proxy(key);
     });
@@ -19,15 +24,28 @@ MVVM.prototype = {
         new Watcher(this, key, cb);
     },
 
+    /* 
+    对指定属性名的属性实现数据代理
+    */
     _proxy: function(key) {
+        // 保存vm
         var me = this;
+        // 给vm添加指定属性名(data中的)的属性, 使用属性描述符
         Object.defineProperty(me, key, {
-            configurable: false,
-            enumerable: true,
+            configurable: false, // 不可重新定义修改
+            enumerable: true, // 可以枚举
+            /* 
+            当通过vm.xxx读取属性值是自动调用
+            */
             get: function proxyGetter() {
+                // 返回data对象中对应属性名的属性值
                 return me._data[key];
             },
+            /* 
+            当通过vm.xxx = value修改属性值时自动调用
+            */
             set: function proxySetter(newVal) {
+                // 将最新的值保存到data对应的属性上
                 me._data[key] = newVal;
             }
         });
